56eea228423b290452a74787646db75e1205f63a
j0t4ku <j.ismar00@gmail.com>
2023-02-27 10:04:48 -0300

    thirth challenge complete

--- myApp.js
+++ myApp.js
@@ -4,18 +4,19 @@ let app = express();
 
 app.get("/", getIndex) 
 
+//first challenge
 function getHandler(req, res){
     res.send("Hello Express");
 }
-
+//second challenge
 function getIndex(req,res){
     absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath)
 }
 
-
-
-
+//thirth challenge
+publicPath = __dirname + '/public';
+app.use("/public",express.static(publicPath));
 
 
 
