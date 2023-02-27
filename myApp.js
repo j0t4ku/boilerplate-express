779ac4564547cb4dc50a591f5d18786ed39ab4ec
j0t4ku <j.ismar00@gmail.com>
2023-02-27 09:49:35 -0300

    Second challenge complete

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
 
+
+app.get("/", getIndex) 
+
+function getHandler(req, res){
+    res.send("Hello Express");
+}
+
+function getIndex(req,res){
+    absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath)
+}
 
 
 
