b4e5aac599587ab758a2bf6b8aaa0365b1aacea9
j0t4ku <j.ismar00@gmail.com>
2023-02-27 10:25:11 -0300

    fiveth challenge complete

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 let express = require('express');
+require('dotenv').config()
 let app = express();
 
 
@@ -18,11 +19,15 @@ function getIndex(req,res){
 publicPath = __dirname + '/public';
 app.use("/public",express.static(publicPath));
 
-//forth challenge
+//forth and five challenge
 app.get("/json",getJson);
 
 function getJson(req,res){
-    res.json({"message": "Hello json"});
+    if(process.env.MESSAGE_STYLE==="uppercase"){
+        res.json({"message": "HELLO JSON"});
+    }else{
+        res.json({"message": "Hello json"});
+    }
 }
 
 
